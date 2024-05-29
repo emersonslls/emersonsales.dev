38443e6ad060258443848a13791dc8b07b12046d
Emerson Sales"git config --global user.name Emerson <emersonsantossales245@gmail.com>
2024-05-29 20:14:53 -0300

    curriculo

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -14,7 +14,7 @@ import Logo from '../../Assets/Logo.png';
 
 /* ===== Curriculo ===== */
 
-import CvEmerson from '../../Assets/CV/CV_emersonsales.pdf';
+import CvEmerson from '../../Assets/CV/CV_emersonsales2.pdf';
 
 /* ===== Icons ===== */
 
